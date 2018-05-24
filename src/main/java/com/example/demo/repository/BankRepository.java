9d73aaec62028ca1e41d3cc96607ece29394334c
Mohamed Ibrahim AK <mohamed.ibrahim@airfi.aero>
2018-05-24 07:29:34 +0530

    Find bank by IFSC code

--- src/main/java/com/example/demo/repository/BankRepository.java
+++ src/main/java/com/example/demo/repository/BankRepository.java
@@ -1,10 +1,18 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Bank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
 
+    @Query(value = "select ifsc, bank_name, branch, state, city, district, address from bank_branches where ifsc = :ifsc ORDER BY ?#{#pageable}",
+            countQuery = "select count(*) from branches where ifsc = :ifsc ORDER BY ?#{#pageable}",
+            nativeQuery = true)
+    Page<Object[]> findByIfsc(@Param("ifsc") String ifsc, Pageable pageable);
 }
